75fc020c78582cf2bcab0b814f8a1237f51eba02
jmagady <josh.magady@gmail.com>
2015-04-13 12:27:50 -0500

    corrected more doc strings, added the configuration items to start setting up user logins

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
-#from flask_sqlalchemy import SQLAlchemy # SQLALCHEMY FLASK WRAPPER
 from flask_sqlalchemy import SQLAlchemy
+import os
+from flask_login import LoginManager
+from flask_openid import OpenID
+from config import basedir
 
-app = Flask(__name__) # creates an instance of flask named app. not be confused with the app dir
-app.config.from_object('config') #tells flask what the name of our config file is/where it is
-db = SQLAlchemy(app) #Creates a SQLAlchemy Database object
-
-
-
-from app import views, models # the . is used inside the package imports the views.py where our routing is handled. Imports models where our database is described
+app = Flask(__name__)  # creates an instance of flask named app. not be confused with the app dir
+app.config.from_object('config')  # tells flask what the name of our config file is/where it is
+db = SQLAlchemy(app)  # Creates a SQLAlchemy Database object
+lm = LoginManager()  # Create a loginmanager instance
+lm.init_app(app)  # initialize with the name of our flask app
+oid = OpenID(app, os.path.join(basedir, 'tmp'))  # initialize OpenID and tell it where it should store its temp files
+from app import views, models
